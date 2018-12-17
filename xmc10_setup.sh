c7f89abe8194a3897500debd0b99e1c5d0169a11
Takao Akaki <mongonta@gmail.com>
2018-12-18 06:18:55 +0900

    bugfix xmc10_setup.sh

--- xmc10_setup.sh
+++ xmc10_setup.sh
@@ -7,7 +7,7 @@ sudo sed -e '$ a blacklist snd_hdmi_lpe_audio' /etc/modprobe.d/blacklist.conf
 sudo cp -rf ./ucm/cht-bsw-rt5672 /usr/share/alsa/ucm
 
 # Suspend 設定
-sudo gsettings set org.gnome.desktop.screensaver. ubuntu-lock-on-suspend false
+sudo gsettings set org.gnome.desktop.screensaver ubuntu-lock-on-suspend false
 sudo cp -f ./scripts/root-resume.service /etc/systemd/system/
 
 sudo systemctl daemon-reload
